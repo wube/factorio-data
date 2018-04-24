e56191c93875499fd2613b1142fd49d62c4fbef5
Tom Mason <wheybags@wheybags.com>
2018-04-24 15:23:06 +0200

    added version 0.15.26

---
+++ core/prototypes/god-controller.lua
@@ -0,0 +1,15 @@
+data:extend(
+{
+  {
+    type = "god-controller",
+    name = "default",
+    
+    crafting_categories = {"crafting"},
+    mining_categories = {"basic-solid"},
+    inventory_size = 60,
+    item_pickup_distance = 1,
+    loot_pickup_distance = 2,
+    movement_speed = 0.5,
+    mining_speed = 0.1
+  }
+})
